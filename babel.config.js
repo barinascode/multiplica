81f843e2cba016b3dee80f92777205078068a454
Leonardo Tapia <barinas.code@gmail.com>
2023-04-05 19:16:36 -0400

    jest, di container typescript setup

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,19 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            // This needs to be mirrored in tsconfig.json
+            '@shared': "./src/shared",
+            '@modules': "./src/modules",
+            src: "./src",
+          },
+        },
+      ],
+      'babel-plugin-transform-typescript-metadata'
+    ],
   };
 };
