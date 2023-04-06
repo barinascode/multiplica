60cc1d3eb2a8ffb5f99ed05726454a0f7f567bb6
Leonardo Tapia <barinas.code@gmail.com>
2023-04-05 20:27:12 -0400

    eslint and code style setup

--- src/jest..setup.screen.test.tsx
+++ src/jest..setup.screen.test.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import { View, Text } from 'react-native';
 
-const TestRenderScreen: React.FC = () => {
-    return <View>
-        <Text testID='message'>Hello World</Text>
-    </View>
-};
+const TestRenderScreen: React.FC = () => <View>
+	<Text testID='message'>Hello World</Text>
+</View>;
 
 // should render the component
 describe('<TestRenderScreen /> ', () => {
-  it('should render the component', () => {
-    const { getByTestId } = render(<TestRenderScreen />);
-    expect(getByTestId('message')).toHaveTextContent('Hello World');
-  });
+	it('should render the component', () => {
+		const { getByTestId } = render(<TestRenderScreen />);
+		expect(getByTestId('message')).toHaveTextContent('Hello World');
+	});
 });
